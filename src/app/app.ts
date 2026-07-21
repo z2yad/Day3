8829b142a22fe5b23a854fd862d4ff8aa2ce7a1d
zeyad salim <ziadsalim121@gmail.com>
2026-07-21 15:05:20 +0300

    feat: implement side-bar component and update Tailwind CSS dependencies

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBar } from './shared/side-bar/side-bar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
