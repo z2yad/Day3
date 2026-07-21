abaf039db5847aa509757c271ba963e83ff73850
zeyad salim <ziadsalim121@gmail.com>
2026-07-21 15:59:20 +0300

    feat: implement dashboard feature and add topbar to main application layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SideBar } from './shared/side-bar/side-bar';
+import { Topbar } from './shared/topbar/topbar';
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SideBar],
+  imports: [RouterOutlet, SideBar, Topbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/app/feature/dashbord/dashbord.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashbord',
+  imports: [],
+  templateUrl: './dashbord.html',
+  styleUrl: './dashbord.css',
+})
+export class Dashbord {
+
+}
